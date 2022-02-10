15ff7a2b50c6a3a18688d76ef9714931c6a0fbe5
Nahan Trindade <nahantrindade.p@gmail.com>
2022-02-09 21:09:51 -0400

    Lógica de execução do jogo

--- JS2/JS2.js
+++ JS2/JS2.js
@@ -7,23 +7,43 @@ function randomNum() {
 function jokenpo(jogador, computador) {
     if (jogador == 2 || computador == 2) {
         if (jogador < computador) {
-            console.log("voce venceu");
             return 1;
         } else {
-            console.log("voce perdeu");
             return 0;
         }
-    }else {
+    } else {
         if (jogador > computador) {
-            console.log("voce venceu");
             return 1;
         } else {
-            console.log("voce perdeu");
             return 0;
         }
     }
 }
 
+let cont = 0;
+let estado = 0;
 
+do {
+    console.log("Escolha sua jogada:\n1 - Papel\n2 - Pedra\n3 - Tesoura");
+    let jogada = parseInt(prompt());
+    let comp = randomNum();
 
+    if (comp == 1) {
+        console.log("O computador jogou Papel\n");
+    }
+    if (comp == 2) {
+        console.log("O computador jogou Pedra\n");
+    }
+    if (comp == 3) {
+        console.log("O computador jogou Tesoura\n");
+    }
 
+    estado = jokenpo(jogada, comp);
+    if (estado == 1) {
+        console.log("Você venceu!");
+        cont++;
+    } else {
+        console.log("Você perdeu! A sua pontuação foi de " + cont);
+    }
+
+} while (estado);
