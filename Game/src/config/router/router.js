a9922cd8706ae20d202b539edd7dfe6b1f516b6a
Nahan Trindade <nahantrindade.p@gmail.com>
2022-04-05 14:13:29 -0400

    Exercicio 6

--- Game/src/config/router/router.js
+++ Game/src/config/router/router.js
@@ -1,9 +1,16 @@
 import express from "express";
 import mainController from "../../../app/controllers/main";
+import areaController from "../../../app/controllers/area";
 const router = express.Router();
 
-router.get("/"              ,mainController.index);
-router.get("/about"         ,mainController.about);
-router.get("/ui"            ,mainController.ui);
-router.get("/game"          ,mainController.game);
+//MainController
+router.get("/"                  , mainController.index);
+router.get("/about"             , mainController.about);
+router.get("/ui"                , mainController.ui);
+router.get("/game"              , mainController.game);
+
+//AreaController
+router.get("/areas"              , areaController.index);
+
+
 export default router;
